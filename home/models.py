4b7a1af508e28e8ec92f2b0f0874a3487983c233
Sajjad Ali <44885230+alikhandkk81@users.noreply.github.com>
2022-06-20 12:55:11 +0100

    Add files via upload

---
+++ home/models.py
@@ -0,0 +1,11 @@
+import email
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    desc = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
